13ede9f354d35210663abab940404fcb2cbe92db
inc0gnit0 <iinc0gnit0@pm.me>
2019-12-07 21:35:17 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install python3
 sudo apt-get install apache2
 sudo apt-get install curl
+sudo npm install -g localtunnel
 sudo apt-get install php libapache2-mod-php
 sudo a2enmod mpm_prefork && sudo a2enmod php7.2
 
