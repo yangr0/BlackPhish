7f9a54f04977a781c1c53944762c55563608aa1a
inc0gnit0 <iinc0gnit0@pm.me>
2020-01-02 18:10:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ sudo a2enmod mpm_prefork && sudo a2enmod php7.2
 # End #
 service apache2 restart
 
-printf "\n\n\033[92;1msudo python3 blackphish.py\033[0m to run\n\n"
+printf "\n\n\To run, use command:\n\n  \033[94;1msudo python3 blackphish.py\n\n"
