b01714648c6fe26a7d0ebe9e89177037c9cd9fa5
inc0gnit0 <iinc0gnit0@pm.me>
2020-01-03 15:26:41 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ sudo apt-get install python3
 sudo apt-get install ssh
 sudo apt-get install apache2
 sudo npm install -g localtunnel
-sudo apt-get install php libapache2-mod-php
 sudo a2enmod mpm_prefork && sudo a2enmod php7.2
 
 # End #
